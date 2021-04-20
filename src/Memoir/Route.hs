1aef5eb3d75acc800ff97b7a4e2224fc9ec52215
Sridhar Ratnakumar <srid@srid.ca>
2021-04-20 18:53:45 -0400

    Use routes like /2021/4/20

--- src/Memoir/Route.hs
+++ src/Memoir/Route.hs
@@ -2,9 +2,8 @@
 
 module Memoir.Route where
 
-import Data.Time (Day)
+import Data.Time (Day, fromGregorianValid, toGregorian)
 import Ema.Route (IsRoute (..), Slug (unSlug))
-import qualified Memoir.Data as Data
 
 data Route
   = Index
@@ -14,8 +13,15 @@ data Route
 instance IsRoute Route where
   toSlug = \case
     Index -> mempty
-    OnDay day -> one $ show day
+    OnDay day ->
+      let (y, m, d) = toGregorian day
+       in [show y, show m, show d]
   fromSlug = \case
     [] -> Just Index
-    [s] -> OnDay <$> Data.parseDay (toString $ unSlug s)
+    [y, m, d] ->
+      OnDay <$> do
+        y' <- readMaybe @Integer (toString $ unSlug y)
+        m' <- readMaybe @Int (toString $ unSlug m)
+        d' <- readMaybe @Int (toString $ unSlug d)
+        fromGregorianValid y' m' d'
     _ -> Nothing
